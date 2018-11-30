7a232b957dad5071be86baf5acfc8f39ed631d78
Yuri Kuzmenko <y.kuzmenko@contact-sys.com>
2018-11-30 17:52:41 +0300

    перевел часть на новый Consumer

--- src/context/localization/local-consumer.js
+++ src/context/localization/local-consumer.js
@@ -3,37 +3,35 @@ import { Consumer } from './local_context'
 
 export default (Component) =>
   class Translater extends ReactComponent {
-
     translate = (dictionary) => (compName) => {
-
       // console.log('LanguageConsumer::translate::value',dictionary)
       // console.log('LanguageConsumer::translate::compName',compName)
       // console.log('LanguageConsumer::translate::keys',Object.keys(dictionary))
       // console.log('LanguageConsumer::translate::values',Object.values(dictionary))
       // console.log('LanguageConsumer::translate::entries',Object.entries(dictionary))
 
-      const vl = Object.entries(dictionary).filter((itr) => itr[0] === compName)[0][1]
-     // console.log('LanguageConsumer::translate::entries[i]',vl)
+      const vl = Object.entries(dictionary).filter(
+        (itr) => itr[0] === compName
+      )[0][1]
+      // console.log('LanguageConsumer::translate::entries[i]',vl)
 
       return vl ? vl : compName
-      
     }
 
     render() {
-
       console.log('LanguageConsumer::render::')
 
       return (
-                <Consumer>
-                  {(dictionary) => {
-                    return(
-                      <Component 
-                        {...this.props} 
-                        getTranslatedValue={this.translate(dictionary)}>
-                      </Component>
-                    )
-                  }}
-                </Consumer>
-             )
+        <Consumer>
+          {(dictionary) => {
+            return (
+              <Component
+                {...this.props}
+                getTranslatedValue={this.translate(dictionary)}
+              />
+            )
+          }}
+        </Consumer>
+      )
     }
   }
